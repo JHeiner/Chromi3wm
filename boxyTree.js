423bc1ea326cc94d6e164b04134c9b8e8a0c52a5
Jeremy Heiner <ScalaProtractor@gmail.com>
2015-01-20 10:38:09 -0500

    polish the boxyTree code
    generate fewer elements. drop useless 'key' attr, inline the key
    function. simplify d3 calls (to move closer to updating the data
    with subsequent calls to get_tree). and add copyright notices.

--- boxyTree.js
+++ boxyTree.js
@@ -1,61 +1,67 @@
 
+// Copyright © 2015, Jeremy Heiner (github.com/JHeiner).
+// All rights reserved. See LICENSE file.
+
+"use strict";
+
 d3.selection.prototype.selectKids = function( selector ) {
-    return this.selectAll( function( d, i ) { return this.children; } )
-	.filter( selector ); }
-
-i3.entries = function( obj ) {
-    return Object.keys( obj ).map( function( k ) {
-	return { key:k, value:obj[k] }; } ); }
-
-i3.keyOrId = function( d, i ) {
-    return d.key || d.id; }
+	return this.selectAll( function( d, i ) { return this.children; } )
+		.filter( selector ); }
 
 i3.fix = function( d, i ) {
-    var n = d3.select( this );
-    switch ( typeof( d ) ) {
-    case 'boolean': case 'number': case 'string':
-	var s = d.toString();
-	n.append( 'span' ).classed( s ? 'literal' : 'missing', true )
-	    .text( s ? s : '(emptystring)' );
-	break;
-    case 'object':
-	if ( ! d ) {
-	    n.append( 'span' ).classed( 'missing', true ).text( '(null)' );
-	    break; }
-	n.classed( Array.isArray( d ) ? 'arr' : 'obj', true );
+	var n = d3.select( this );
+	switch ( typeof( d ) ) {
+	case 'boolean': case 'number': case 'string':
+		var s = d.toString();
+		n.classed( s ? 'literal' : 'missing', true );
+		n.text( s ? s : '(emptystring)' );
+		break;
+	case 'object':
+		if ( ! d ) {
+			n.classed( 'missing', true );
+			n.text( '(null)' );
+			break; }
 
-	var t = d3.select( this ).selectKids( 'div.table' )
-	    .data( [''] );
-	t.enter().append( 'div' ).classed( 'tbl', true );
+		var arr = Array.isArray( d );
+		n.classed( arr ? 'arr' : 'obj', true );
 
-	var rs = t.selectKids( 'div.row' )
-	    .data( i3.entries( d ), i3.keyOrId );
-	rs.enter().append( 'div' ).classed( 'row', true )
-	    .attr( 'key', i3.keyOrId )
-	    .each( function( d, i ) {
-		var r = d3.select( this );
-		r.append( 'span' ).classed( 'key', true ).text( d.key );
-		r.append( 'div' ).classed( 'val', true ).datum( d.value ); } );
+		var t = n.selectKids( 'div.tbl' ).data( [''] );
+		t.enter().append( 'div' ).classed( 'tbl', true );
 
-	rs.selectKids( '.val' ).each( i3.fix );
+		var es = Object.keys( d ).map( function( k ) {
+			return { key : k, value : d[k] }; } );
 
-	break;
-    default:
-	n.append( 'span' ).classed( 'missing', true ).text( '('+typeof(d)+')' );
-	break; } }
+		var rs = t.selectKids( 'div.row' )
+			.data( es, function( d, i ) { return d.key; } );
+		rs.enter().append( 'div' ).classed( 'row', true )
+			.each( function( d, i ) {
+				var r = d3.select( this );
+				if ( ! arr ) r.classed( 'key-'+d.key, true );
+				r.append( 'span' ).classed( 'key', true ).text( d.key );
+				r.append( 'span' ).classed( 'val', true ).datum( d.value ); } );
+		rs.selectKids( '.val' ).each( i3.fix );
+
+		break;
+	default:
+		n.classed( 'missing', true );
+		n.text( '('+typeof(d)+')' );
+		break; } }
 
 i3.listener = function( data ) {
-    var i3 = window.i3;
-    i3.close();
-    i3.data = data;
-    d3.select( '#time' ).text( Date() )
+	var i3 = window.i3;
+	i3.close();
 
-    i3.root = d3.select( '#boxy' ).selectKids( 'div' )
-	.data( [data], i3.keyOrId );
-    i3.root.enter().append( 'div' ).attr( 'key', i3.keyOrId );
+	i3.data = data;
+	i3.time = d3.select( '#time' );
+	i3.boxy = d3.select( '#boxy' );
 
-    i3.root.each( i3.fix );
+	i3.time.text( Date() );
+	i3.boxy.datum( data );
+	i3.boxy.each( i3.fix );
 }
 i3.open();
 i3.ask(4);
 
+// Local Variables:
+// tab-width: 4
+// End:
